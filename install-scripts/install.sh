a52802368f9eef437d654159ae9f15786495b9c9
Sven Boor <sven@transbits.nl>
2023-09-17 08:10:38 +0000

    Added microhubs controller and zone stats collector.

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -8,8 +8,4 @@ echo "Wait for 5s to let postgresql start up."
 sleep 5
 ./prepare_db.sh
 
-docker compose -f docker-compose.db.yaml \
-    -f docker-compose.api-services.yaml \
-    -f docker-compose.data-services.yaml \
-    -f docker-compose.gateway.yaml \
-    up -d
+./run.sh
---
+++ install-scripts/run.sh
@@ -0,0 +1,5 @@
+docker compose -f docker-compose.db.yaml \
+    -f docker-compose.api-services.yaml \
+    -f docker-compose.data-services.yaml \
+    -f docker-compose.gateway.yaml \
+    up -d
