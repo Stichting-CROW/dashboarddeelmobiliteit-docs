aef2b937a30e09711825a54cbe342f63f88a4d41
Sven Boor <sven.boor@gmail.com>
2023-09-27 08:31:09 +0200

    Added new approach for deploying front.

--- install-scripts/run.sh
+++ install-scripts/run.sh
@@ -4,5 +4,4 @@ docker compose -f docker-compose.db.yaml \
     -f docker-compose.api-services.yaml \
     -f docker-compose.data-services.yaml \
     -f docker-compose.gateway.yaml \
-    -f docker-compose.frontend.yaml \
     up -d
