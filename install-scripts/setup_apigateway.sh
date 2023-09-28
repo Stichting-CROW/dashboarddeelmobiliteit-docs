748ff4a67e5c87adda2de8d4ba93b6ebcec46481
Sven Boor <sven.boor@gmail.com>
2023-09-28 05:49:15 +0200

    Improvements setup apigateway.

--- install-scripts/setup_apigateway.sh
+++ install-scripts/setup_apigateway.sh
@@ -35,80 +35,80 @@ curl --request POST \
   --url http://localhost:8001/consumers \
   --header 'Content-Type: application/json' \
   --header 'accept: application/json' \
-  --data '{"username":"deny","custom_id":"deny","tags":["general"]}'
+  --data '{"username":"anonymous_consumer","custom_id":"anonymous_consumer","tags":["general"]}'
 
+ curl -sX POST localhost:8001/consumers/anonymous_consumer/plugins/ \
+   -H "Content-Type: application/json" \
+   --data '{"name": "request-termination", "config": { "status_code": 401, "content_type": "application/json; charset=utf-8", "body": "{\"error\": \"Authentication required\"}"} }'
 
 # Setup dashboard api
-curl -i -s -X POST http://localhost:8001/services \
+curl -s -X POST http://localhost:8001/services \
   --data name=dashboard-api \
   --data url='http://dashboard-api:8000'
 
-curl -i -X POST http://localhost:8001/services/dashboard-api/routes \
+curl -s -X POST http://localhost:8001/services/dashboard-api/routes \
   --data 'paths[]=/dashboard-api' \
   --data name=dashboard-api-route
 
-curl -i -s -X POST http://localhost:8001/services \
+curl -s -X POST http://localhost:8001/services \
   --data name=dashboard-api-public \
   --data url='http://dashboard-api:8000/public'
 
-curl -i -X POST http://localhost:8001/services/dashboard-api-public/routes \
+curl -X POST http://localhost:8001/services/dashboard-api-public/routes \
   --data 'paths[]=/dashboard-api/public' \
-  --data name=dashboard-api-public-route
-
+  --data name=dashboard-api-public-route 
+  
 curl -X POST http://localhost:8001/routes/dashboard-api-route/plugins \
-    --data "name=acl"  \
-    --data "config.deny=anonymous"  
+  --data "name=jwt" \
+  --data "config.anonymous=anonymous_consumer"
 
+curl -X POST http://localhost:8001/route/dashboard-api-route/plugins \
+    --data "name=key-auth"  \
+    --data "config.key_names=apikey" \
+    --data "config.anonymous=anonymous_consumer"
 
 # 2 microhubs api
 # admin + MDS
-curl -i -s -X POST http://localhost:8001/services \
+curl -s -X POST http://localhost:8001/services \
   --data name=microhubs-api \
   --data url='http://policy-api:80'
 
-curl -i -X POST http://localhost:8001/services/microhubs-api/routes \
+curl -X POST http://localhost:8001/services/microhubs-api/routes \
   --data "hosts[]=${MDS_URL}" \
   --data 'paths[]=/admin' \
   --data name=microhubs-admin \
   --data strip_path=false
 
-curl -i -X POST http://localhost:8001/services/microhubs-api/routes \
+curl -X POST http://localhost:8001/services/microhubs-api/routes \
   --data "hosts[]=${MDS_URL}" \
   --data 'paths[]=/' \
   --data name=mds-public
 
-curl -X POST http://localhost:8001/routes/microhubs-admin/plugins \
-    --data "name=acl"  \
-    --data "config.deny=anonymous" 
-
 # 3 od api
-curl -i -s -X POST http://localhost:8001/services \
+curl -s -X POST http://localhost:8001/services \
   --data name=od-api \
   --data url='http://od-api:80'
 
-curl -i -X POST http://localhost:8001/services/od-api/routes \
+curl -X POST http://localhost:8001/services/od-api/routes \
   --data "hosts[]=${API_PROXY_URL}" \
   --data 'paths[]=/od-api' \
   --data name=od-api-route
 
+
 curl -X POST http://localhost:8001/routes/od-api-route/plugins \
-    --data "name=acl"  \
-    --data "config.deny=anonymous"
+    --data "name=jwt"
 
 #4 admin api
 
-curl -i -s -X POST http://localhost:8001/services \
+curl -s -X POST http://localhost:8001/services \
   --data name=admin-api \
   --data url='http://admin-api:80'
 
-curl -i -X POST http://localhost:8001/services/admin-api/routes \
+curl -X POST http://localhost:8001/services/admin-api/routes \
   --data "hosts[]=${API_PROXY_URL}" \
   --data 'paths[]=/admin' \
   --data name=admin-api-route
 
-curl -X POST http://localhost:8001/routes/admin-api-route/plugins \
-    --data "name=acl"  \
-    --data "config.deny=anonymous" 
 
 # Restart everything with changed environment variables.
 ./run.sh
