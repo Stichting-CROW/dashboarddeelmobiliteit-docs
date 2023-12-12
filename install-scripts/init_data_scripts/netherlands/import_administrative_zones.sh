3cfbc1966f4f0e9fa840012ac17143600222e044
Sven Boor <sven.boor@gmail.com>
2023-12-12 10:27:57 +0100

    Import administrative zones.

--- install-scripts/init_data_scripts/netherlands/import_administrative_zones.sh
+++ install-scripts/init_data_scripts/netherlands/import_administrative_zones.sh
@@ -6,6 +6,5 @@ export $(grep -v '^#' ../../.env | xargs -d '\n')
 wget https://service.pdok.nl/cbs/wijkenbuurten/2022/atom/downloads/wijkenbuurten_2022_v1.gpkg
 
 # Add shapes from municipalities, residential_areas en neighborhoods to zones.
-ogr2ogr -f PostgreSQL "PG:dbname=dashboarddeelmobiliteit user=postgres port=5432 host=localhost" wijkenbuurten_2022_v1.gpkg
+ogr2ogr -t_srs "EPSG:4326" -f PostgreSQL "PG:dbname=dashboarddeelmobiliteit user=postgres port=5432 host=localhost" wijkenbuurten_2022_v1.gpkg --config OGR_TRUNCATE YES
 psql -U postgres -d dashboarddeelmobiliteit -f insert_in_zones.sql -h localhost
-
