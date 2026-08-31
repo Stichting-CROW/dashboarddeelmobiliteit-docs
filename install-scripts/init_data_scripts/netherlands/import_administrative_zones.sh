4a9850dec47f9d18b6472b7c9885589b79346d3b
bartwr <mail@bartroorda.nl>
2026-08-31 17:00:35 +0200

    Update 'CBS Wijken en Buurten' to latest PDOK 2026

--- install-scripts/init_data_scripts/netherlands/import_administrative_zones.sh
+++ install-scripts/init_data_scripts/netherlands/import_administrative_zones.sh
@@ -2,9 +2,9 @@
 export $(grep -v '^#' ../../.env | xargs -d '\n')
 
 # Download from PDOK
-# https://www.pdok.nl/atom-downloadservices/-/article/cbs-wijken-en-buurten#3af4dfc5c041640dbd8e17c9b69c8356
-wget https://service.pdok.nl/cbs/wijkenbuurten/2022/atom/downloads/wijkenbuurten_2022_v1.gpkg
+# https://www.pdok.nl/-/nieuwe-dataset-cbs-wijken-en-buurten-2026-beschikbaar-bij-pdok
+wget https://service.pdok.nl/cbs/wijken-en-buurten-2026/atom/downloads/WijkBuurtkaart_2026_v0.gpkg
 
 # Add shapes from municipalities, residential_areas en neighborhoods to zones.
-ogr2ogr -t_srs "EPSG:4326" -f PostgreSQL "PG:dbname=dashboarddeelmobiliteit user=postgres port=5432 host=localhost" wijkenbuurten_2022_v1.gpkg --config OGR_TRUNCATE YES
+ogr2ogr -t_srs "EPSG:4326" -f PostgreSQL "PG:dbname=dashboarddeelmobiliteit user=postgres port=5432 host=localhost" WijkBuurtkaart_2026_v0.gpkg --config OGR_TRUNCATE YES
 psql -U postgres -d dashboarddeelmobiliteit -f insert_in_zones.sql -h localhost
--- install-scripts/init_data_scripts/netherlands/insert_in_zones.sql
+++ install-scripts/init_data_scripts/netherlands/insert_in_zones.sql
@@ -1,15 +1,15 @@
 INSERT INTO zones (area, name, owner, municipality, zone_type, stats_ref)
-SELECT geom, gemeentenaam, null, gemeentecode, 'municipality', concat('cbs:', gemeentecode)
+SELECT geom, gm_naam, null, gm_code, 'municipality', concat('cbs:', gm_code)
 FROM gemeenten
 WHERE water = 'NEE';
 
 INSERT INTO zones (area, name, owner, municipality, zone_type, stats_ref)
-SELECT geom, buurtnaam, null, gemeentecode, 'neighborhood', concat('cbs:', buurtcode)
+SELECT geom, bu_naam, null, gm_code, 'neighborhood', concat('cbs:', bu_code)
 FROM buurten
 WHERE water = 'NEE';
 
 INSERT INTO zones (area, name, owner, municipality, zone_type, stats_ref)
-SELECT geom, wijknaam, null, gemeentecode, 'residential_area', concat('cbs:', wijkcode)
+SELECT geom, wk_naam, null, gm_code, 'residential_area', concat('cbs:', wk_code)
 FROM wijken
 WHERE water = 'NEE';
 
